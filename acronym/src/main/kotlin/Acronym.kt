c1a5e1f51c6391a4f14510e80fc295df9005483c
Lothar Schulz <lothar.sch@gmx.de>
2018-04-23 23:10:18 +0200

    first instead of string slicing on position zero

--- acronym/src/main/kotlin/Acronym.kt
+++ acronym/src/main/kotlin/Acronym.kt
@@ -2,7 +2,7 @@ class Acronym{
     companion object {
         fun generate (phrase: String): String {
             return phrase.split(" ", ":", "-", ",").
-                    filter { it.isNotBlank() }.map { it[0].toUpperCase() }.joinToString("")
+                    filter { it.isNotBlank() }.map { it.first().toUpperCase() }.joinToString("")
         }
     }
 }
