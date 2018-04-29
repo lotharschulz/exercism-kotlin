af0877b838e196820a5701bebc73cf5653cdfd77
Lothar Schulz <lothar.sch@gmx.de>
2018-04-29 23:09:22 +0200

    kotlin 1.2.41 and scrabble test all tests passing

---
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -0,0 +1,20 @@
+
+
+class ScrabbleScore{
+    companion object {
+        private fun valueOf(letter: Char): Int {
+            return when (letter.toUpperCase()) {
+                'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' ->  1
+                'D', 'G'                                         ->  2
+                'B', 'C', 'M', 'P'                               ->  3
+                'F', 'H', 'V', 'W', 'Y'                          ->  4
+                'K'                                              ->  5
+                'J', 'X'                                         ->  8
+                'Q', 'Z'                                         -> 10
+                else                                             ->  0
+            }
+        }
+
+        fun scoreWord(word: String): Int = word.sumBy { it -> valueOf(it) }
+    }
+}
