74a758c554228dfd20c6bd76771d31dcbb893e9c
Lothar Schulz <lothar.sch@gmx.de>
2018-05-29 23:21:30 +0200

    toBinary refeactored

--- secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,17 +1,18 @@
 class HandshakeCalculator{
     companion object {
         fun calculateHandshake(decimalNumber: Int): List<Signal>{
-            var binNumber: String = ""
-            println(toBinary(decimalNumber, binNumber))
+            println(toBinary(decimalNumber))
             return listOf<Signal>(Signal.WINK)
         }
 
-        fun toBinary(decimalNumber: Int, binaryString: String = "") : String {
-            while (decimalNumber > 0) {
-                val temp = "${binaryString}${decimalNumber%2}"
-                return toBinary(decimalNumber/2, temp)
+        fun toBinary(decimalNumber: Int) : String {
+            val result = StringBuilder()
+            var decNmbr = decimalNumber
+            while (decNmbr>0){
+                result.append(decimalNumber%2)
+                decNmbr /= 2
             }
-            return binaryString.reversed()
+            return result.reversed().toString()
         }
     }
 }
