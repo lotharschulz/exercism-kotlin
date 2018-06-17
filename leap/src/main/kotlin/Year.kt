682a0f0af12a4e703636bc3a9c1dad2e3bacb976
Lothar Schulz <lothar.sch@gmx.de>
2018-06-17 13:25:06 +0200

    when expression and short constructor parameter

--- leap/src/main/kotlin/Year.kt
+++ leap/src/main/kotlin/Year.kt
@@ -1,25 +1,8 @@
-class Year constructor(private val year: Int){
-
-    var isLeap: Boolean = false
-    init {
-/*
-        when (year.rem(400)){
-            0 -> isLeap = true
-        }
-*/
-        if (year % 400 == 0) isLeap = true
-        else if (year % 100 == 0) isLeap = false
-        else if (year % 4 == 0) isLeap = true
-/*
-        if (year % 4 == 0){
-            isLeap = true
-            if (year % 100 == 0){
-                isLeap = false
-                if (year % 400 == 0){
-                    isLeap = true
-                }
-            }
-        }
-*/
+class Year constructor(year: Int){
+    val isLeap: Boolean = when{
+        year % 400 == 0 -> true
+        year % 100 == 0 -> false
+        year % 4 == 0 -> true
+        else -> false
     }
 }
