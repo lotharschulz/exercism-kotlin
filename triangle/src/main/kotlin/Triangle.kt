c03e037dde1e6bc7ccbf49947634f13148f1b846
Lothar Schulz <lothar.sch@gmx.de>
2018-07-21 07:50:57 +0200

    cleanup

--- triangle/src/main/kotlin/Triangle.kt
+++ triangle/src/main/kotlin/Triangle.kt
@@ -2,8 +2,6 @@ class Triangle (x: Double, y: Double, z: Double) {
     constructor(x: Int, y: Int, z: Int) : this(x.toDouble(), y.toDouble(), z.toDouble())
 
     init {
-//        println("x: $x, y: $y, z: $z")
-//        println("$x + $y > $z: " + (x + y > z) )
         require(x > 0 && y > 0 && z > 0 )
             { "All three sides must be greater than 0, actually the side values are x: '$x', y: '$y', z: '$z'" }
         require(x + y > z && x + z > y && y + z > x )
