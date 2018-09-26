8a7408fcd8769a26a4ad756432ae880b9a81e23b
Lothar Schulz <lothar.sch@gmx.de>
2018-09-26 21:44:15 +0200

    simplified solution

--- flatten-array/src/main/kotlin/Flattener.kt
+++ flatten-array/src/main/kotlin/Flattener.kt
@@ -1,15 +1,9 @@
 object Flattener {
-    fun flatten (l: List<Any?>):List<Any?>{
-        val flatList = mutableListOf<Any?>()
-        flattenList(l, flatList)
-        return flatList
-    }
-
-    private fun flattenList(nestedList: List<Any?>, flatList: MutableList<Any?>) {
-        nestedList.forEach{
-            e -> when(e){
-                !is List<Any?> -> if(null != e) flatList.add(e)
-                else -> flattenList(e, flatList)
+    fun flatten(nestedList: List<Any?>):List<Any?> {
+        return nestedList.filterNotNull().flatMap{
+            when(it){
+                is List<*> -> flatten(it)
+                else -> listOf(it)
             }
         }
     }
