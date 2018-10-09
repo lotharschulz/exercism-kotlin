afdf19fd426d4fe18d3b8de0c2cf13486d91df4f
Lothar Schulz <lothar.sch@gmx.de>
2018-10-10 00:01:19 +0200

    code restructured

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -1,3 +1,14 @@
 object WordCount{
-    fun phrase(s: String): Map<String, Int> = s.toLowerCase().replace("(\\W'|'\\W|\\.|:|,|[!%@&\$^])".toRegex(), " ").trim().split("\\s+".toRegex()).groupingBy { it }.eachCount() // can't use \\p{Punct} unfortunately
+    private const val NORMALIZED_REG_EXP = "(\\W'|'\\W|\\.|:|,|[!%@&\$^])"
+
+    fun phrase (s: String): Map<String, Int> = wordsList(s).groupingBy { it }.eachCount()
+
+    private fun wordsList(s: String): List<String> = normalizeString(s).split("\\s+".toRegex())
+
+    private fun normalizeString(s: String): String = s.toLowerCase()
+            .replace(NORMALIZED_REG_EXP.toRegex(), " ").trim()
+
+    // one line version
+    // fun phrase(s: String): Map<String, Int> = s.toLowerCase().replace("(\\W'|'\\W|\\.|:|,|[!%@&\$^])".toRegex(), " ").trim().split("\\s+".toRegex()).groupingBy { it }.eachCount() // can't use \\p{Punct} unfortunately
+
 }
