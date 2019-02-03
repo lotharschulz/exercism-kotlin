156653618fd7dbcdcba7ff51d57623b7ed5683cf
Lothar Schulz <lothar.sch@gmx.de>
2019-02-03 17:37:06 +0100

    while loop bug fixed

--- robot-name/src/main/kotlin/Robot.kt
+++ robot-name/src/main/kotlin/Robot.kt
@@ -1,20 +1,27 @@
-import java.util.*
+import java.util.Random
 
 // https://stackoverflow.com/a/45687695
 fun IntRange.random() = Random().nextInt((endInclusive + 1) - start) +  start
 fun String.double() = this + this
 
 class Robot{
-    var name:String = generateName()
+    private val names = mutableSetOf<String>()
+    var name:String = generateAndAddName()
+
+    private fun generateName():String = generateChar().toString().double() + generateDigits()
+
+    private fun generateAndAddName():String{
+        names.add(generateName())
+        return names.last()
+    }
+
+    private fun generateDigits():String = (100..999).random().toString()
+    private fun generateChar():Char = (65..90).random().toChar()
 
     fun reset(){
         do {
-            val newname = generateName()
-            name = newname
-        }while(!names.add(newname))
+            name = generateName()
+        }while(names.contains(name))
     }
-    private val names:MutableSet<String> = mutableSetOf()
-    private fun generateName():String = generateChar().toString().double() + generateDigits()
-    private fun generateDigits():String = (100..999).random().toString()
-    private fun generateChar():Char = (65..90).random().toChar()
+
 }
