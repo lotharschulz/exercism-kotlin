cc43d7bca615d127b20f10defed56a99fc9584b0
Lothar Schulz <lothar.sch@gmx.de>
2019-03-27 06:46:57 +0100

    implementation scribbles

--- largest-series-product/src/main/kotlin/Series.kt
+++ largest-series-product/src/main/kotlin/Series.kt
@@ -1,5 +1,21 @@
 class Series(private val input: String) {
-    fun getLargestProduct(digits: Int):Int {
+
+    fun numberSegments(digits: Int):List<List<Number>>{
+        require(digits <= input.length) {"Input string length must be bigger than digit number."}
+        if(digits == 0) return listOf(listOf(1))
+
+        val inputIntList:List<Int> = input.toList().map { it.toInt() - 48 }
+        println("inputIntList: $inputIntList")
+
+        return listOf(listOf(1,2))
+    }
+
+    fun transformInputToIntList(input: String):List<Number>{
+        // val inputIntList:List<Int> = input.toList().map { it.toInt() - 48 }
+        return listOf(1,2)
+    }
+
+    fun getLargestProduct(digits: Int):Number {
         require(input.matches("\\d+?".toRegex())) { "Input must be a number" }
 
         if (input.isBlank())  return 0
@@ -30,5 +46,8 @@ class Series(private val input: String) {
 
 fun main(){
     val s = Series("123456")
+
+    println(s.numberSegments(3))
+
     println(s.getLargestProduct(3))
 }
