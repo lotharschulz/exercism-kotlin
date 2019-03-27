8ff6b2d40d0c25ac80801700fd9dd52e27772ff7
Lothar Schulz <lothar.sch@gmx.de>
2019-03-27 23:01:17 +0100

    implementation ctnd

--- largest-series-product/src/main/kotlin/Series.kt
+++ largest-series-product/src/main/kotlin/Series.kt
@@ -1,23 +1,38 @@
 class Series(private val input: String) {
 
-    fun numberSegments(digits: Int):List<List<Number>>{
+    fun segments(digits: Int):List<List<Int>>{
         require(digits <= input.length) {"Input string length must be bigger than digit number."}
         if(digits == 0) return listOf(listOf(1))
 
-        val inputIntList:List<Int> = input.toList().map { it.toInt() - 48 }
-        println("inputIntList: $inputIntList")
+        val inputIntList:List<Int> = transformInputToIntList(input)
 
-        return listOf(listOf(1,2))
+        var j = 0
+        val l = mutableListOf<List<Int>>()
+
+        while (j + digits <= inputIntList.size){
+            l.add(inputIntList.subList(j, j + digits))
+            j += 1
+        }
+        return l
     }
 
-    fun transformInputToIntList(input: String):List<Number>{
-        // val inputIntList:List<Int> = input.toList().map { it.toInt() - 48 }
-        return listOf(1,2)
+    private fun transformInputToIntList(input: String):List<Int>{
+        return input.toList().map { it.toInt() - 48 }
     }
 
     fun getLargestProduct(digits: Int):Number {
-        require(input.matches("\\d+?".toRegex())) { "Input must be a number" }
+        println("digits: $digits")
+        require(input.matches("\\d+?".toRegex())) { "digits must be a number." }
+        var product:Int = 0
+        var p:Int
+        for (s in segments(digits)){
+            p = s.reduce { acc, i -> acc * i }
+            if ( p > product ) product = p
+        }
+        return product
+            
 
+/*
         if (input.isBlank())  return 0
         var numbers = mutableListOf<Int>()
         for (s in input){
@@ -34,20 +49,21 @@ class Series(private val input: String) {
 
         val sum2 = numbers.take(digits).reduce { acc, i -> acc * i }
         println(sum2)
+*/
 
 /*
         input.foldRight(1,
             mul, next -> mul * next
         )
         */
-      return 9 * digits
+//      return 9 * digits
     }
 }
 
 fun main(){
-    val s = Series("123456")
+    val s = Series("1027839564")
 
-    println(s.numberSegments(3))
+    println(s.segments(3))
 
     println(s.getLargestProduct(3))
 }
--- largest-series-product/src/test/kotlin/SeriesTest.kt
+++ largest-series-product/src/test/kotlin/SeriesTest.kt
@@ -10,61 +10,51 @@ class SeriesTest {
         assertEquals(18, Series("29").getLargestProduct(2))
     }
 
-    @Ignore
     @Test
     fun findsTheLargestProductOf2WithNumbersInOrder() {
         assertEquals(72, Series("0123456789").getLargestProduct(2))
     }
 
-    @Ignore
     @Test
     fun findsTheLargestProductOf2() {
         assertEquals(48, Series("576802143").getLargestProduct(2))
     }
 
-    @Ignore
     @Test
     fun findsTheLargestProductOf3WithNumbersInOrder() {
         assertEquals(504, Series("0123456789").getLargestProduct(3))
     }
 
-    @Ignore
     @Test
     fun findsTheLargestProductOf3() {
         assertEquals(270, Series("1027839564").getLargestProduct(3))
     }
 
-    @Ignore
     @Test
     fun findsTheLargestProductOf5WithNumbersInOrder() {
         assertEquals(15120, Series("0123456789").getLargestProduct(5))
     }
 
-    @Ignore
     @Test
     fun findsTheLargestProductWithinABigNumber() {
         assertEquals(23520, Series("73167176531330624919225119674426574742355349194934").getLargestProduct(6))
     }
 
-    @Ignore
     @Test
     fun reports0IfAllDigitsAre0() {
         assertEquals(0, Series("0000").getLargestProduct(2))
     }
 
-    @Ignore
     @Test
     fun reports0IfAllSpansInclude0() {
         assertEquals(0, Series("99099").getLargestProduct(3))
     }
 
-    @Ignore
     @Test(expected = IllegalArgumentException::class)
     fun rejectsSpanLongerThanStringLength() {
         Series("123").getLargestProduct(4)
     }
 
-    @Ignore
     @Test
     fun reports1ForEmptyStringAndEmptyProduct() {
         assertEquals(1, Series("").getLargestProduct(0))
