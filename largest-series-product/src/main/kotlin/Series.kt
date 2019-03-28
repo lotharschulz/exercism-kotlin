cfe5ca09916d789fd096708dce501309d2d35ed9
Lothar Schulz <lothar.sch@gmx.de>
2019-03-29 00:04:11 +0100

    improved implementation

--- largest-series-product/src/main/kotlin/Series.kt
+++ largest-series-product/src/main/kotlin/Series.kt
@@ -1,38 +1,18 @@
 class Series(private val input: String) {
 
     init {
-        require(input.isEmpty() || input.matches("\\d+?".toRegex())) { "Input must be a number." }
-    }
-
-    private fun segments(digits: Int):List<List<Int>>{
-        require(digits <= input.length) {"Input string length must be bigger than digit number."}
-        if(digits == 0) return listOf(listOf(1))
-
-        val inputIntList:List<Int> = transformInputToIntList(input)
-
-        var j = 0
-        val l = mutableListOf<List<Int>>()
-
-        while (j + digits <= inputIntList.size){
-            l.add(inputIntList.subList(j, j + digits))
-            j += 1
-        }
-        return l
-    }
-
-    private fun transformInputToIntList(input: String):List<Int>{
-        return input.toList().map { it.toInt() - 48 }
+        require(input.all { it.isDigit() }) { "Input must contain numbers only." }
     }
 
     fun getLargestProduct(digits: Int):Number {
         require(digits >= 0){ "Digits must be greater or equal zero." }
-        if (digits == 0) return 1
-        var product = 0
-        var p:Int
-        for (s in segments(digits)){
-            p = s.reduce { acc, i -> acc * i }
-            if ( p > product ) product = p
-        }
-        return product
+        require(digits <= input.length) {"Input string length must be bigger than digit number."}
+
+        if (digits == input.length)
+            return input.fold(1){ acc, next -> acc * (next.toInt() - 48)}
+        return (0..(input.length - digits))
+                .map { input.slice(it..(it + digits-1)) }
+                .map { it.fold(1){ acc, next -> acc * (next.toInt() - 48)} }
+                .max()!!
     }
 }
