02f47144c34e4baf2d73d0d3e9f486cc831f665c
Lothar Schulz <lothar.sch@gmx.de>
2019-04-09 22:37:38 +0200

    enable remaining tests

--- rotational-cipher/src/test/kotlin/RotationalCipherTest.kt
+++ rotational-cipher/src/test/kotlin/RotationalCipherTest.kt
@@ -1,4 +1,4 @@
-import org.junit.Ignore
+
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -16,56 +16,48 @@ class RotationalCipherTest {
         assertEquals("b", cipher.encode("a"))
     }
 
-    @Ignore
     @Test
     fun testRotateLowercaseABy26SingleWrapAround() {
         val cipher = RotationalCipher(26)
         assertEquals("a", cipher.encode("a"))
     }
 
-    @Ignore
     @Test
     fun testRotateLowercaseMBy13NoWrapAround() {
         val cipher = RotationalCipher(13)
         assertEquals("z", cipher.encode("m"))
     }
 
-    @Ignore
     @Test
     fun testRotateLowercaseNBy1SingleWrapAround() {
         val cipher = RotationalCipher(13)
         assertEquals("a", cipher.encode("n"))
     }
 
-    @Ignore
     @Test
     fun testRotateCapitalLettersNoWrapAround() {
         val cipher = RotationalCipher(5)
         assertEquals("TRL", cipher.encode("OMG"))
     }
 
-    @Ignore
     @Test
     fun testSpacesAreUnalteredByRotation() {
         val cipher = RotationalCipher(5)
         assertEquals("T R L", cipher.encode("O M G"))
     }
 
-    @Ignore
     @Test
     fun testNumbersAreUnalteredByRotation() {
         val cipher = RotationalCipher(4)
         assertEquals("Xiwxmrk 1 2 3 xiwxmrk", cipher.encode("Testing 1 2 3 testing"))
     }
 
-    @Ignore
     @Test
     fun testPunctuationIsUnalteredByRotation() {
         val cipher = RotationalCipher(21)
         assertEquals("Gzo'n zvo, Bmviyhv!", cipher.encode("Let's eat, Grandma!"))
     }
 
-    @Ignore
     @Test
     fun testAllLettersRotateCorrectly() {
         val cipher = RotationalCipher(13)
