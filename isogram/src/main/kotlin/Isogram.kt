d385bc6f3b58d74ebfc2eece5c8b8fc513e9084e
Lothar Schulz <lothar.sch@gmx.de>
2019-06-28 09:31:53 +0200

    isogram 2nd iteration

--- isogram/src/main/kotlin/Isogram.kt
+++ isogram/src/main/kotlin/Isogram.kt
@@ -1,4 +1,4 @@
 object Isogram{
     fun isIsogram(input: String): Boolean =
-      0 == input.toLowerCase().filter { it.isLetterOrDigit() }.groupingBy { it }.eachCount().filter { it.value > 1 }.size
+      !input.toLowerCase().filter { it.isLetterOrDigit() }.groupingBy { it }.eachCount().any { it.value > 1 }
 }
