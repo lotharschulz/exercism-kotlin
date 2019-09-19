16b6027776d29bc2aa96fb7e33f1abd0d9dcd9f3
Lothar Schulz <lothar.schulz@moovel.com>
2019-09-19 18:00:45 +0200

    BinarySearch all tests passing iterative & recursive

--- binary-search/src/main/kotlin/BinarySearch.kt
+++ binary-search/src/main/kotlin/BinarySearch.kt
@@ -1,5 +1,40 @@
 object BinarySearch {
-    fun search(word: List<Int>, definition: Int): Int{
-        return 0
+    const val INVALID = -1
+
+    fun search(list: List<Int>, search: Int):Int{
+        require(list.isSorted())
+        if (list.size == 1) return 0
+        if (list.isEmpty() || list.elementAt(0) > search || !list.contains(search)) return INVALID
+        return binSearchRecursive(list,search,0, list.size)
+        //return binSearchIterative(list, search)
     }
+
+    private fun binSearchRecursive(list: List<Int>, search: Int, startIndex: Int, endIndex: Int): Int{
+        val m = (startIndex+endIndex)/2
+        return when {
+            search > list.elementAt(m) -> binSearchRecursive(list, search,m+1, endIndex)
+            search < list.elementAt(m) -> binSearchRecursive(list, search, startIndex, m-1)
+            else -> return m // search == list[m]
+        }
+    }
+
+    private fun binSearchIterative(list: List<Int>, search: Int): Int{
+        require(list.isSorted())
+
+        var r = 0.rangeTo(list.lastIndex)
+        var m: Int
+
+        while (!r.isEmpty()){
+            m = ( r.start + r.endInclusive ) / 2
+            when {
+                search > list[m] -> { r = m + 1..list.lastIndex }
+                search < list[m] -> { r = 0 until m }
+                list[m] == search -> return m
+            }
+        }
+        return INVALID
+    }
+
+
+    private fun <T: Comparable<T>> List<T>.isSorted(): Boolean = (0 until lastIndex).none() { e -> this[e+1] <= this[e] }
 }
