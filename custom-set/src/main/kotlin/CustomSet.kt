c06b0099347fd5b728d112a85650f74aed5d07a1
Lothar Schulz <lothar.schulz@moovel.com>
2019-12-27 17:06:18 +0100

    feat:
    implementation based on mutable list value

--- custom-set/src/main/kotlin/CustomSet.kt
+++ custom-set/src/main/kotlin/CustomSet.kt
@@ -1,84 +1,16 @@
-import java.util.HashMap
+class CustomSet (vararg ds: Int){
+    private val d = mutableListOf<Int>()
+    init { d.addAll(ds.distinct()) }
 
-class CustomSet (vararg xs: Int){
+    fun isEmpty(): Boolean = d.isEmpty()
+    fun isSubset(other: CustomSet): Boolean = d.all { other.contains(it) }
+    fun isDisjoint(other: CustomSet): Boolean = this.isEmpty() || this.intersection(other).isEmpty()
+    fun contains(other: Int): Boolean = d.contains(other)
+    fun intersection(other: CustomSet): CustomSet = CustomSet(*(this.d.filter { other.sData().contains(it) }.toIntArray()))
+    fun add(other: Int) { if (!d.contains(other)) d.add(other) }
+    override fun equals(other: Any?): Boolean = if (other !is CustomSet) false else this.sData().toIntArray() contentEquals other.sData().toIntArray()
+    operator fun plus(other: CustomSet): CustomSet = CustomSet(*((sData() + other.sData()).distinct().toIntArray()))
+    operator fun minus(other: CustomSet): CustomSet = CustomSet(*((sData() - other.sData()).toIntArray()))
 
-    private val container = hashMapOf<Int, Int>()
-    //private val Object PRESENT = new Object();
-
-    init {
-        for (x in xs){
-            container[x] = x
-        }
-    }
-
-    fun isEmpty(): Boolean {
-        return container.isEmpty()
-    }
-
-    fun isSubset(other: CustomSet): Boolean {
-        for (x in container){
-            if (!other.contains(x.value)){
-                return false
-            }
-        }
-        return true
-    }
-
-    fun isDisjoint(other: CustomSet): Boolean {
-        return this.isEmpty() || this.intersection(other).isEmpty()
-    }
-
-    fun contains(other: Int): Boolean {
-        return container.containsValue(other)
-    }
-
-    private fun keys():Set<*>{
-        return container.keys
-    }
-
-    fun intersection(other: CustomSet): CustomSet {
-        if (this.container.isEmpty()) return CustomSet()
-        val intersectionContainer = mutableMapOf<Int, Int> ()
-        for (k in other.keys()){
-            if (k is Int && this.contains(k)){
-                intersectionContainer[k] = k
-            }
-        }
-        return CustomSet(*(intersectionContainer.keys.toIntArray()))
-    }
-
-    fun add(other: Int) {
-        container[other] = other
-    }
-
-    override fun equals(other: Any?): Boolean {
-        val o = other as CustomSet
-        if (o.keys().size != this.keys().size) return false
-        for (x in container.keys){
-            if (!o.container.contains(x)) return false
-        }
-        return true
-}
-
-operator fun plus(other: CustomSet): CustomSet {
-val addContainer = container.toMutableMap<Int, Int> ()
-for (x in other.keys()){
-    if (x is Int) {
-        if (!addContainer.contains(x)) {
-            addContainer[x] = x
-        }
-    }
-}
-return CustomSet(*(addContainer.keys.toIntArray()))
-}
-
-operator fun minus(other: CustomSet): CustomSet {
-val minContainer = container.toMutableMap<Int, Int> ()
-for (x in other.keys()){
-    if (x is Int && minContainer.contains(x)) {
-        minContainer.remove(x)
-    }
-}
-return CustomSet(*(minContainer.keys.toIntArray()))
-}
+    private fun sData(): List<Int> = d.toList().sorted()
 }
