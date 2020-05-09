0aa279b48ec5b82ac24cda1ac802546eef96ffb3
Lothar Schulz <lothar.sch@gmx.de>
2020-05-09 21:54:01 +0200

    when replaces if

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -2,16 +2,15 @@ class Matrix(private val matrixAsString: String) {
 
     private val NL = "\n"
     private val SPACE = " "
+    private val oneLiner = matrixAsString.indexOf(NL) == -1
 
-    fun column(colNr: Int): List<Int> =
-        if (matrixAsString.indexOf(NL) == -1)
-            listOf(matrixAsString.toInt())
-        else
-            matrixAsString.split(NL).map { it.split(SPACE).filter { it.isNotEmpty() }.get(colNr-1).toInt() }
+    fun column(colNr: Int): List<Int> = when(oneLiner){
+        true -> listOf(matrixAsString.toInt())
+        false -> matrixAsString.split(NL).map { it.split(SPACE).filter { it.isNotEmpty() }.get(colNr-1).toInt() }
+    }
 
-    fun row(rowNr: Int): List<Int> =
-        if (matrixAsString.indexOf(NL) == -1)
-            listOf(matrixAsString.toInt())
-        else
-            matrixAsString.split(NL).get(rowNr-1).split(SPACE).filter { it.isNotEmpty() }.map { it.toInt() }
+    fun row(rowNr: Int): List<Int> = when(oneLiner){
+        true -> listOf(matrixAsString.toInt())
+        false -> matrixAsString.split(NL).get(rowNr-1).split(SPACE).filter { it.isNotEmpty() }.map { it.toInt() }
+    }
 }
