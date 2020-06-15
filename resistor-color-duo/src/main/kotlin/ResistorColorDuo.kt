e1be82f11674c251f4c3e2778669a82cf99fd7e9
Lothar Schulz <lothar.sch@gmx.de>
2020-06-15 23:09:01 +0200

    resistor-color-duo

---
+++ resistor-color-duo/src/main/kotlin/ResistorColorDuo.kt
@@ -0,0 +1,23 @@
+object ResistorColorDuo {
+
+    val colormap: Map<Color, Int> = mapOf(
+            Color.BLACK to 0,
+            Color.BROWN to 1,
+            Color.RED to 2,
+            Color.ORANGE to 3,
+            Color.YELLOW to 4,
+            Color.GREEN to 5,
+            Color.BLUE to 6,
+            Color.VIOLET to 7,
+            Color.GREY to 8,
+            Color.WHITE to 9
+    ).withDefault {
+        10
+    }
+
+    fun value(vararg colors: Color): Int =
+        colors.take(2)
+            .map { colormap.getValue(it) }
+            .joinToString("")
+            .toInt()
+}
