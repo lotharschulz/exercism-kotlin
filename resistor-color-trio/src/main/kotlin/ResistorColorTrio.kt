c5851476969b95fc0acd3df140fc5b0ebee56639
Lothar Schulz <lothar.sch@gmx.de>
2020-06-24 22:59:23 +0200

    chore: working 3rd solution

--- resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -1,8 +1,10 @@
+import kotlin.math.log
+import kotlin.math.log10
 import kotlin.math.pow
 
 object ResistorColorTrio {
 
-    val colors: Map<Color, Int> = mapOf(
+    private val colors: Map<Color, Int> = mapOf(
             Color.BLACK to 0,
             Color.BROWN to 1,
             Color.RED to 2,
@@ -17,7 +19,7 @@ object ResistorColorTrio {
         10
     }
 
-    val decimals: Map<Int, String> = mapOf(
+    private val decimals: Map<Int, String> = mapOf(
             1 to " ",
             10 to "0 ",
             100 to " kilo",
@@ -28,18 +30,44 @@ object ResistorColorTrio {
         "error"
     }
 
-    fun secondPosition(i: Color): String{
+    private fun secondPosition(i: Color): String{
         if (colors[i] == 0) return ""
         else if (colors[i] == 7) return "70"
         else return colors[i].toString()
     }
 
-    fun decimalMultiple(input: Color): String? =
+    private fun magnitude(input: Color): String? =
             decimals[10.toDouble().pow( colors[input] ?: error(-1)).toInt()]
 
     fun text(vararg input: Color): String =
-            input[0].ordinal.toString() + secondPosition(input[1]) + decimalMultiple(input[2]) + "ohms"
+            input[0].ordinal.toString() + secondPosition(input[1]) + magnitude(input[2]) + "ohms"
 
     fun text2(vararg input: Color): String =
-            colors[input[0]].toString() + secondPosition(input[1]) + decimalMultiple(input[2]) + "ohms"
+            colors[input[0]].toString() + secondPosition(input[1]) + magnitude(input[2]) + "ohms"
+
+    private fun resistorValue(vararg input: Color): Int {
+        val (c1, c2, c3) = input
+        var value = 10 * c1.ordinal + c2.ordinal
+        repeat(times = c3.ordinal) { value *= 10 }
+        return value
+    }
+
+    fun text3(vararg input: Color): String {
+        val value = resistorValue(*input)
+        //var magnitude = 0
+        val mag3 = log(value.toDouble(), 1000.toDouble()).toInt()
+        val newvalue = (value / 1000.toDouble().pow(mag3.toDouble())).toInt()
+/*
+        println("value: $value - newvalue: $newvalue - magnitude: $magnitude -- mag3: $mag3")
+        while (value % 1000 == 0) {
+            println("value: $value")
+            value /= 1000
+            magnitude++
+        }
+        println("value: $value - newvalue: $newvalue - magnitude: $magnitude -- mag3: $mag3")
+*/
+
+        val unit = Unit.values()[mag3].name.toLowerCase()
+        return "$newvalue $unit"
+    }
 }
--- resistor-color-trio/src/test/kotlin/ResistorColorTrioTest.kt
+++ resistor-color-trio/src/test/kotlin/ResistorColorTrioTest.kt
@@ -11,17 +11,51 @@ class ResistorColorTrioTest {
     fun `orange orange black 2`() = assertEquals("33 ohms", ResistorColorTrio.text2(ORANGE, ORANGE, BLACK))
 
     @Test
+    fun `orange orange black 3`() = assertEquals("33 ohms", ResistorColorTrio.text3(ORANGE, ORANGE, BLACK))
+
+    @Test
     fun `blue grey brown`() = assertEquals("680 ohms", ResistorColorTrio.text(BLUE, GREY, BROWN))
+
+    @Test
+    fun `blue grey brown2`() = assertEquals("680 ohms", ResistorColorTrio.text2(BLUE, GREY, BROWN))
+
+    @Test
+    fun `blue grey brown3`() = assertEquals("680 ohms", ResistorColorTrio.text3(BLUE, GREY, BROWN))
 
     @Test
     fun `red black red`() = assertEquals("2 kiloohms", ResistorColorTrio.text(RED, BLACK, RED))
 
     @Test
+    fun `red black red2`() = assertEquals("2 kiloohms", ResistorColorTrio.text2(RED, BLACK, RED))
+
+    @Test
+    fun `red black red3`() = assertEquals("2 kiloohms", ResistorColorTrio.text3(RED, BLACK, RED))
+
+    @Test
     fun `green brown orange`() = assertEquals("51 kiloohms", ResistorColorTrio.text(GREEN, BROWN, ORANGE))
+
+    @Test
+    fun `green brown orange2`() = assertEquals("51 kiloohms", ResistorColorTrio.text2(GREEN, BROWN, ORANGE))
+
+    @Test
+    fun `green brown orange3`() = assertEquals("51 kiloohms", ResistorColorTrio.text3(GREEN, BROWN, ORANGE))
 
     @Test
     fun `yellow violet yellow`() = assertEquals("470 kiloohms", ResistorColorTrio.text(YELLOW, VIOLET, YELLOW))
 
     @Test
+    fun `yellow violet yellow2`() = assertEquals("470 kiloohms", ResistorColorTrio.text2(YELLOW, VIOLET, YELLOW))
+
+    @Test
+    fun `yellow violet yellow3`() = assertEquals("470 kiloohms", ResistorColorTrio.text3(YELLOW, VIOLET, YELLOW))
+
+    @Test
     fun `yellow violet violet`() = assertEquals("470 megaohms", ResistorColorTrio.text(YELLOW, VIOLET, VIOLET))
+
+    @Test
+    fun `yellow violet violet2`() = assertEquals("470 megaohms", ResistorColorTrio.text2(YELLOW, VIOLET, VIOLET))
+
+    @Test
+    fun `yellow violet violet3`() = assertEquals("470 megaohms", ResistorColorTrio.text3(YELLOW, VIOLET, VIOLET))
+
 }
