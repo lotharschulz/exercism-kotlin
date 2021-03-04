fe22327241693ee97b118112fcf0291087fdf7a5
Lothar Schulz <lothar.sch@gmx.de>
2021-03-04 22:59:41 +0100

    run length encoding

--- run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
+++ run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -1,15 +1,19 @@
 object RunLengthEncoding {
 
-    fun encode(input: String): String =
-        input.group().joinToString("") { if (it.isEmpty()) "" else it.length.toString() + it.last() }
+    fun encode(input: String): String = input.group().joinToString("") {
+            when{
+                it.isEmpty() -> ""
+                it.length == 1 -> it
+                else -> it.length.toString() + it.last()
+            }
+        }
 
-    fun decode(input: String): String {
-        TODO("Implement this to complete the task")
-    }
+    fun decode(input: String): String = input.replace("(\\d+)(\\D)".toRegex()) {
+            it.groupValues[2].repeat(it.groupValues[1].toInt())
+        }
 
     private fun String.group(): List<String> =
         if (isEmpty()) listOf()
         else listOf(takeWhile { it == first() }) + dropWhile { it == first() }.group()
-
 
 }
