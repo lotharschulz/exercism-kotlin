979b1a6c92e3a3fdd3e14a087f823d151cc8b877
Lothar Schulz <lothar.sch@gmx.de>
2021-03-21 11:09:38 +0100

    chore: wordy passes all tests

--- wordy/src/main/kotlin/Wordy.kt
+++ wordy/src/main/kotlin/Wordy.kt
@@ -1,6 +1,20 @@
 object Wordy {
+    private val ops = "(plus|minus|multiplied by|divided by)".toRegex()
+    private val opx = "$ops (-?[0-9]+)|(raised to) the ([0-9]+)th power".toRegex()
+    private val exp = "What is (-?[0-9]+)( $ops (-?[0-9]+)| raised to the ([0-9]+)th power)*[?]".toRegex()
 
     fun answer(input: String): Int {
-        TODO("Implement this to complete the task")
+        val init = exp.find(input)?.groupValues?.get(1)?.toInt() ?: throw Exception()
+        val operations = opx.findAll(input).map(MatchResult::groupValues)
+        return operations.fold(init) { acc, op ->
+            when (op[1]) {
+                "plus"          -> acc + op[2].toInt()
+                "minus"         -> acc - op[2].toInt()
+                "multiplied by" -> acc * op[2].toInt()
+                "divided by"    -> acc / op[2].toInt()
+                ""              -> List(op.last().toInt()) { acc }.reduce(Int::times)
+                else            -> throw Exception()
+            }
+        }
     }
 }
