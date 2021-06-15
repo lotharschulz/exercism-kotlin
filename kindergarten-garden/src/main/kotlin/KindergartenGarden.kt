0075547714468acf4f6c6f8de8250e64ea35859e
Lothar Schulz <lothar.sch@gmx.de>
2021-06-15 10:22:05 +0200

    chore initial implementation: first two test succeed

--- kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
+++ kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -1,6 +1,23 @@
 class KindergartenGarden(private val diagram: String) {
 
+    val cupToPlants = mapOf(
+        "V" to "violets",
+        "R" to "radishes",
+        "C" to "clover",
+        "G" to "grass"
+    )
+
+    val studentsInClass = listOf<String>("Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry")
+
     fun getPlantsOfStudent(student: String): List<String> {
-        TODO("Implement the function to complete the task")
+        require(studentsInClass.contains(student))
+        val index = studentsInClass.indexOf(student)
+        val diagramList: List<String> = diagram.split("\n")
+        val plantsList = mutableListOf<String>();
+        diagramList.forEach {
+            cupToPlants[it.get(index*2).toString()]?.let { cupLabel -> plantsList.add(cupLabel) }
+            cupToPlants[it.get(index*2+1).toString()]?.let { cupLabel -> plantsList.add(cupLabel) }
+        }
+        return plantsList
     }
 }
