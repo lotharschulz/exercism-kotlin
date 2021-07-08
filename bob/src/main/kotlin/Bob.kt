22d2be7e9d54985aa54903faf35ad00aa17685cc
Lothar Schulz <lothar.sch@gmx.de>
2021-07-08 18:53:14 +0200

    chore bob all tests pass

--- bob/src/main/kotlin/Bob.kt
+++ bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,17 @@
 object Bob {
-    fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
-    }
+    fun hey(input: String): String = when{
+            input.noContent() -> "Fine. Be that way!"
+            input.isQuestion() && input.isYell() -> "Calm down, I know what I'm doing!"
+            input.isQuestion() -> "Sure."
+            input.isYell() -> "Whoa, chill out!"
+            else -> "Whatever."
+        }
+
+    private val PUNCTUATION_REGEX = "[!\"#\$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex();
+    private fun String.stripLastChar(): String = this.slice(0..this.length - 2)
+    private fun String.isPunctuations(): Boolean = PUNCTUATION_REGEX.containsMatchIn( this.stripLastChar().filter { !it.isWhitespace() } )
+
+    private fun String.isQuestion(): Boolean = !this.noContent() && this.isNotEmpty() && this.trim().last() == '?'
+    private fun String.isYell(): Boolean = this.stripLastChar().any { it.isLetter() } && this.stripLastChar().filter { it.isLetter() }.all { it.isUpperCase() }
+    private fun String.noContent(): Boolean = this.none { it.isLetterOrDigit() } && !this.isPunctuations()
 }
