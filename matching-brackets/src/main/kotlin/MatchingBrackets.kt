c9932743314d4d682ce7bd0e8ffa4f9e1b1cbcba
Lothar Schulz <lothar.sch@gmx.de>
2021-07-15 00:06:58 +0200

    chore first attempt, not tested yet

--- matching-brackets/src/main/kotlin/MatchingBrackets.kt
+++ matching-brackets/src/main/kotlin/MatchingBrackets.kt
@@ -1,6 +1,19 @@
+import java.lang.Character.MIN_VALUE as nullChar
 object MatchingBrackets {
 
     fun isValid(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val stack = Deque<Char>()
+        input.forEach {
+            val lastStackChar = stack.peek() ?: nullChar
+            when{
+                !it.isBracket() -> return@forEach
+                stack.isNotEmpty() && it.matchesBracket(lastStackChar) -> stack.pop()
+                else -> stack.push(it)
+            }
+        }
+        return stack.isEmpty()
     }
+
+    private fun Char.isBracket(): Boolean = this in listOf('[', ']', '{', '}', '(', ')')
+    private fun Char.matchesBracket(c: Char): Boolean = this == c
 }
