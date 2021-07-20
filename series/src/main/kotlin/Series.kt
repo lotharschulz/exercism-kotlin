0cb8c47f1b8053258cddd8ad48fa4eb57d532353
Lothar Schulz <lothar.sch@gmx.de>
2021-07-20 07:57:42 +0200

    chore series - all tests pass

--- series/src/main/kotlin/Series.kt
+++ series/src/main/kotlin/Series.kt
@@ -1,6 +1,9 @@
 object Series {
 
     fun slices(n: Int, s: String): List<List<Int>> {
-        TODO("Implement this function to complete the task")
+        require(s.isNotEmpty() && n <= s.length)
+        val r = mutableListOf<List<Int>>()
+        s.windowed(n).map { substr -> r.add(substr.toCharArray().toList().map { numberChar -> numberChar.code - 48 }) }
+        return r.toList()
     }
 }
