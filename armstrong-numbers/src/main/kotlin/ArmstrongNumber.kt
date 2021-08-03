cdc29b1f0439421a17da8378069cde0af7fc0b57
Lothar Schulz <lothar.sch@gmx.de>
2021-08-03 22:19:23 +0200

    chore armstrong numbers all tests pass see also https://github.com/lotharschulz/armstrongNumbers/tree/main/kotlin

--- armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
+++ armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
@@ -1,7 +1,32 @@
 object ArmstrongNumber {
 
-    fun check(input: Int): Boolean {
-        TODO("Implement the function to complete the task")
+    fun check(input: Int): Boolean =
+        when {
+            input > -1 -> input == getDigitsRecursive(input).sumOf { digit ->
+                Math.pow(digit.toDouble(), getNumberOfDigitsLog(input).toDouble())
+            }.toInt()
+            else -> false
+        }
+
+    fun getDigitsRecursive(input: Int): List<Int> {
+        if (input == 0) return listOf(0)
+        val result = mutableListOf<Int>()
+        getDigitsRecursiveAcc(input, result)
+        return result.toList()
     }
 
+    fun getDigitsRecursiveAcc(input: Int, acc: MutableList<Int>): List<Int> =
+        when {
+            input > 0 -> {
+                acc.add(input % 10)
+                getDigitsRecursiveAcc(input / 10, acc)
+            }
+            else -> {
+                acc.reverse()
+                acc
+            }
+        }
+
+    fun getNumberOfDigitsLog(input: Int): Int =
+        if (input == 0) 1 else Math.log10(input.toDouble()).toInt() + 1
 }
--- armstrong-numbers/src/test/kotlin/ArmstrongNumberTest.kt
+++ armstrong-numbers/src/test/kotlin/ArmstrongNumberTest.kt
@@ -32,4 +32,37 @@ class ArmstrongNumberTest {
 
     @Test
     fun `seven digit number that is not an armstrong number`() = assertFalse(ArmstrongNumber.check(9926314))
+
+    @Test
+    fun `test getDigitsRecursive`() {
+        assertEquals(listOf(5, 8, 3, 7), ArmstrongNumber.getDigitsRecursive(5837))
+        assertEquals(listOf(4, 3, 2, 1), ArmstrongNumber.getDigitsRecursive(4321))
+        assertEquals(listOf<Int>(), ArmstrongNumber.getDigitsRecursive(-1))
+        assertEquals(listOf<Int>(0), ArmstrongNumber.getDigitsRecursive(0))
+    }
+
+    @Test
+    fun `test getDigitsRecursiveAcc`() {
+        val digits1 = listOf(5, 8, 3, 7)
+        val result1 = mutableListOf<Int>()
+        ArmstrongNumber.getDigitsRecursiveAcc(5837, result1)
+        assertEquals(digits1, result1.toList())
+        val digits2 = listOf<Int>()
+        val result2 = mutableListOf<Int>()
+        ArmstrongNumber.getDigitsRecursiveAcc(0, result2)
+        assertEquals(digits2, result2.toList())
+        val result3 = mutableListOf<Int>()
+        ArmstrongNumber.getDigitsRecursiveAcc(-1, result3)
+        assertEquals(digits2, result3.toList())
+    }
+
+    @Test
+    fun `test getNumberOfDigitsLog`() {
+        val one = 1
+        assertEquals(one, ArmstrongNumber.getNumberOfDigitsLog(0))
+        assertEquals(one, ArmstrongNumber.getNumberOfDigitsLog(1))
+        assertEquals(one, ArmstrongNumber.getNumberOfDigitsLog(-1))
+        assertEquals(4, ArmstrongNumber.getNumberOfDigitsLog(1243))
+        assertEquals(6, ArmstrongNumber.getNumberOfDigitsLog(100000))
+    }
 }
