87ee4bb80f4886c764caccbae91c99c8dd3c495f
Lothar Schulz <lothar.sch@gmx.de>
2021-09-13 19:29:04 +0200

    chore meetup all tests passing

--- meetup/src/main/kotlin/Meetup.kt
+++ meetup/src/main/kotlin/Meetup.kt
@@ -1,11 +1,23 @@
 import java.time.DayOfWeek
 import java.time.LocalDate
 
-class Meetup {
-
-    // TODO: implement proper constructor
-
-    fun day(dayOfWeek: DayOfWeek, schedule: MeetupSchedule): LocalDate{
-        TODO("Implement this function to complete the task")
+class Meetup(private val month: Int, private val year: Int) {
+    private val monthDays = generateSequence(LocalDate.of(year, month, 1)) { localDate ->
+        localDate.plusDays(1).takeIf { it.month.value == month }
     }
+
+    fun day(dayOfWeek: DayOfWeek, schedule: MeetupSchedule): LocalDate = LocalDate.of(year, month,
+        monthDays
+            .filter { it.dayOfWeek == dayOfWeek }
+            .let { days ->
+                when(schedule){
+                    MeetupSchedule.LAST -> days.last()
+                    MeetupSchedule.TEENTH -> days.first { day ->
+                        val teenthDayOfMonth = 13..19
+                        day.dayOfMonth in teenthDayOfMonth
+                    }
+                    else -> days.drop(schedule.ordinal).first() // in MeetupSchedule.FIRST..MeetupSchedule.FOURTH
+                }
+            }.dayOfMonth
+    )
 }
