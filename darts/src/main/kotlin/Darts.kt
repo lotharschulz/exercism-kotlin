1f3d8787d82990443200ff4228aed2b245708707
Lothar Schulz <lothar.sch@gmx.de>
2023-02-02 11:19:35 +0100

    chore darts solved

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,7 +1,15 @@
+import kotlin.math.abs
+import kotlin.math.hypot
+
 object Darts {
 
     fun score(x: Number, y: Number): Int {
-        // @TODO: implement it
-        return 0
+        val deviation = hypot(abs(x.toDouble()), abs(y.toDouble()))
+        return when {
+            deviation <= 1 -> 10
+            deviation <= 5 -> 5
+            deviation <= 10 -> 1
+            else -> 0
+        }
     }
 }
