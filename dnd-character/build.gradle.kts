45eb7514d1aae8b0bb42f441d7a068913ce88e60
Lothar Schulz <lothar.sch@gmx.de>
2023-02-03 08:07:34 +0100

    chore dnd added

---
+++ dnd-character/build.gradle.kts
@@ -0,0 +1,22 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit:junit:4.13.2")
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.withType<Test> {
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events("passed", "failed", "skipped")
+    }
+}
