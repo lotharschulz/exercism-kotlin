05176cd5743a74dc33246b1f4afdaeabc92e160a
Lothar Schulz <lothar.sch@gmx.de>
2023-02-04 15:51:46 +0100

    chore dnd character passed

--- dnd-character/src/main/kotlin/DndCharacter.kt
+++ dnd-character/src/main/kotlin/DndCharacter.kt
@@ -1,22 +1,19 @@
+import kotlin.random.Random
+
 class DndCharacter {
 
-    val strength: Int = TODO("Initialize value to complete the task")
-    val dexterity: Int = TODO("Initialize value to complete the task")
-    val constitution: Int = TODO("Initialize value to complete the task")
-    val intelligence: Int = TODO("Initialize value to complete the task")
-    val wisdom: Int = TODO("Initialize value to complete the task")
-    val charisma: Int = TODO("Initialize value to complete the task")
-    val hitpoints: Int = TODO("Initialize value to complete the task")
+    val strength: Int = ability()
+    val dexterity: Int = ability()
+    val constitution: Int = constitution()
+    val intelligence: Int = ability()
+    val wisdom: Int = ability()
+    val charisma: Int = ability()
+    val hitpoints: Int = getHitpoints(constitution)
 
     companion object {
-
-        fun ability(): Int {
-            TODO("Implement the function to complete the task")
-        }
-
-        fun modifier(score: Int): Int {
-            TODO("Implement the function to complete the task")
-        }
+        fun constitution(): Int = Random.nextInt(3, 18)
+        fun ability(): Int = Random.nextInt(3, 18)
+        fun modifier(score: Int): Int = Math.floorDiv(score - 10, 2)
+        fun getHitpoints(constitution: Int): Int = 10 + modifier(constitution)
     }
-
 }
