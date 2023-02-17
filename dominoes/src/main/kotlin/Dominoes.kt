9ca6d36ec9536db4a803c5b60cdaa3d239505732
Lothar Schulz <lothar.sch@gmx.de>
2023-02-17 17:46:45 +0100

    chore tests pass

--- dominoes/src/main/kotlin/Dominoes.kt
+++ dominoes/src/main/kotlin/Dominoes.kt
@@ -4,7 +4,26 @@ data class Domino(val left: Int, val right: Int)
 
 object Dominoes {
 
-    fun formChain(inputDominoes: List<Domino>): List<Domino> {
-        TODO("Implement this to complete the task")
+    fun formChain(vararg xs: Domino) = formChain(xs.toList())
+
+    fun formChain(xs: List<Domino>): List<Domino> = when {
+        xs.isEmpty() -> emptyList()
+        else -> {
+            val ys = mutableListOf(listOf(xs.first()))
+            order(ys, xs.drop(1))
+            ys.removeIf { it.size != xs.size || it.first().left != it.last().right }
+            if (ys.isEmpty()) throw ChainNotFoundException("empty list") else ys.first()
+        }
+    }
+
+    private fun order(acc: MutableList<List<Domino>>, pool: List<Domino>) {
+        if (pool.isEmpty()) return
+        val xs = acc.last()
+        val last = xs.last()
+        pool.filter { last.right == it.left || last.right == it.right }
+            .forEach {
+                acc.add(xs.plus(if (last.right == it.left) it else Domino(it.right, it.left)))
+                order(acc, pool.minus(it))
+            }
     }
 }
