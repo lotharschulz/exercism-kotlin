5495092690d70983a2a7810ccbafc668d77f8e9d
Lothar Schulz <lothar.sch@gmx.de>
2023-09-07 21:49:41 +0200

    chore first 4 binary search tree tests passed

--- binary-search-tree/src/main/kotlin/BinarySearchTree.kt
+++ binary-search-tree/src/main/kotlin/BinarySearchTree.kt
@@ -1,11 +1,16 @@
 class BinarySearchTree<T : Comparable<T>> {
 
-    data class Node<T>(val data: T /* maybe more... */)
+    data class Node<T>(val data: T, val left: Node<T>? = null, val right: Node<T>? = null)
 
     var root: Node<T>? = null
 
     fun insert(value: T) {
-        TODO("Delete this statement and write your own implementation.")
+        root = root?.insert(value) ?: Node(value)
+    }
+
+    private fun Node<T>.insert(value: T): Node<T> = when {
+        value <= this.data -> this.copy(left = this.left?.insert(value) ?: Node(value))
+        else -> this.copy(right = this.right?.insert(value) ?: Node(value))
     }
 
     fun asSortedList(): List<T> {
@@ -15,5 +20,4 @@ class BinarySearchTree<T : Comparable<T>> {
     fun asLevelOrderList(): List<T> {
         TODO("Delete this statement and write your own implementation.")
     }
-
 }
