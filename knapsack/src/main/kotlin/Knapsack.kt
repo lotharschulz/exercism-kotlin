d254da9f51a88c041ee805d169058812729743cc
Lothar Schulz <lothar.sch@gmx.de>
2023-09-19 23:37:10 +0200

    chore one 5 items test fail, one five item test fail

--- knapsack/src/main/kotlin/Knapsack.kt
+++ knapsack/src/main/kotlin/Knapsack.kt
@@ -1,5 +1,26 @@
 data class Item(val weight: Int, val value: Int)
 
 fun knapsack(maximumWeight: Int, items: List<Item>): Int {
-    TODO("Implement this function to complete the task")
+    val sortedItems = items.sortedWith(compareBy({ it.value }, { it.value })).reversed()
+
+    /*
+    println("sortedItems: $sortedItems")
+    sortedItems.forEachIndexed { index, item ->
+        val aggregatedSublistWeight = sortedItems.subList(0, index).map { it.weight }.sum()
+        val aggregatedSublistValue = sortedItems.subList(0, index).map { it.value }.sum()
+        println("aggregatedSublistWeight: $aggregatedSublistWeight")
+        println("aggregatedSublistValue: $aggregatedSublistValue")
+        if ()
+    }
+    */
+    var aggregatedWeight = 0
+    var aggregatedValue = 0
+    for(item in sortedItems) {
+        aggregatedWeight += item.weight
+        aggregatedValue += item.value
+        if (aggregatedWeight > maximumWeight) {
+            return aggregatedValue - item.value
+        }
+    }
+    return 0
 }
