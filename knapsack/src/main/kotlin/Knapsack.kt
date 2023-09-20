c1458598b05b8f0290ea0b433c814b07f388cb12
Lothar Schulz <lothar.sch@gmx.de>
2023-09-20 22:02:59 +0200

    chore one test still failing

--- knapsack/src/main/kotlin/Knapsack.kt
+++ knapsack/src/main/kotlin/Knapsack.kt
@@ -5,8 +5,8 @@ fun knapsack(maximumWeight: Int, items: List<Item>): Int {
         return 0
     }
     var maxValue = 0
-    items.filter { it.weight <= maximumWeight }.forEach { item ->
-        val subList = items.toList().filter { it != item }
+    items.filter { it.weight <= maximumWeight }.forEachIndexed { index, item ->
+        val subList = items.subList(0, index) + items.subList(index + 1, items.size)
         val attempt = item.value + knapsack(maximumWeight - item.weight, subList)
         maxValue = maxOf(maxValue, attempt)
     }
