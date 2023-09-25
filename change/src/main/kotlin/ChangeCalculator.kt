bafad6c7a0709baeae3973335c964c54f210e491
Lothar Schulz <lothar.sch@gmx.de>
2023-09-25 21:54:35 +0200

    chore change challenge all tests pass

--- change/src/main/kotlin/ChangeCalculator.kt
+++ change/src/main/kotlin/ChangeCalculator.kt
@@ -1,6 +1,39 @@
-class ChangeCalculator(val l: List<Int>) {
+// class ChangeCalculator(val l: List<Int>) {
+//
+//    fun computeMostEfficientChange(grandTotal: Int): List<Int> {
+//        return listOf(25)
+//    }
+// }
+class ChangeCalculator(private val l: List<Int>) {
 
     fun computeMostEfficientChange(grandTotal: Int): List<Int> {
-        return listOf(25)
+        require(grandTotal >= 0) { "Negative totals are not allowed." }
+
+        if (grandTotal == 0) return listOf()
+
+        return computeChange(grandTotal)
+            ?: throw IllegalArgumentException("The total $grandTotal cannot be represented in the given currency.")
+    }
+
+    private fun computeChange(change: Int): List<Int>? {
+        val sortedCoins: List<Int> = l.sortedDescending()
+        require(sortedCoins.all { it > 0 }) { "Coin values must be positive." }
+        val sortedCoinsUsed = Array(change + 1) { Int.MAX_VALUE }
+        sortedCoinsUsed[0] = 0
+
+        val changeCombinations = Array(change + 1) { mutableListOf<Int>() }
+
+        for (coin in sortedCoins) {
+            for (changeAmount in coin..change) {
+                if (sortedCoinsUsed[changeAmount - coin] != Int.MAX_VALUE &&
+                    sortedCoinsUsed[changeAmount - coin] + 1 < sortedCoinsUsed[changeAmount]
+                ) {
+                    sortedCoinsUsed[changeAmount] = sortedCoinsUsed[changeAmount - coin] + 1
+                    changeCombinations[changeAmount] = (changeCombinations[changeAmount - coin] + coin).toMutableList()
+                }
+            }
+        }
+
+        return changeCombinations.last().takeIf { it.sum() == change }
     }
 }
--- change/src/test/kotlin/ChangeCalculatorTest.kt
+++ change/src/test/kotlin/ChangeCalculatorTest.kt
@@ -1,4 +1,3 @@
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
@@ -16,57 +15,51 @@ class ChangeCalculatorTest {
         assertContainsExactly(computedChange, listOf(25))
     }
 
-    @Ignore
     @Test
     fun multipleCoinChange() {
         val computedChange = ChangeCalculator(listOf(1, 5, 10, 25, 100)).computeMostEfficientChange(15)
         assertContainsExactly(computedChange, listOf(5, 10))
     }
 
-    @Ignore
     @Test
     fun changeWithLilliputianCoins() {
         val computedChange = ChangeCalculator(listOf(1, 4, 15, 20, 50)).computeMostEfficientChange(23)
         assertContainsExactly(computedChange, listOf(4, 4, 15))
     }
 
-    @Ignore
     @Test
     fun changeWithLowerElboniaCoins() {
         val computedChange = ChangeCalculator(listOf(1, 5, 10, 21, 25)).computeMostEfficientChange(63)
         assertContainsExactly(computedChange, listOf(21, 21, 21))
     }
 
-    @Ignore
     @Test
     fun largeTargetValues() {
         val computedChange = ChangeCalculator(listOf(1, 2, 5, 10, 20, 50, 100)).computeMostEfficientChange(999)
-        assertContainsExactly(computedChange,
-            listOf(2, 2, 5, 20, 20, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100))
+        assertContainsExactly(
+            computedChange,
+            listOf(2, 2, 5, 20, 20, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100),
+        )
     }
 
-    @Ignore
     @Test
     fun possibleChangeWithoutUnitCoinsAvailable() {
         val computedChange = ChangeCalculator(listOf(2, 5, 10, 20, 50)).computeMostEfficientChange(21)
         assertContainsExactly(computedChange, listOf(2, 2, 2, 5, 10))
     }
 
-    @Ignore
     @Test
     fun anotherPossibleChangeWithoutUnitCoinsAvailable() {
         val computedChange = ChangeCalculator(listOf(4, 5)).computeMostEfficientChange(27)
         assertContainsExactly(computedChange, listOf(4, 4, 4, 5, 5, 5))
     }
 
-    @Ignore
     @Test
     fun noCoinsMake0Change() {
         val computedChange = ChangeCalculator(listOf(1, 5, 10, 21, 25)).computeMostEfficientChange(0)
         assertEquals(0, computedChange.size)
     }
 
-    @Ignore
     @Test
     fun errorTestingForChangeSmallerThanTheSmallestCoin() {
         val changeCalculator = ChangeCalculator(listOf(5, 10))
@@ -77,7 +70,6 @@ class ChangeCalculatorTest {
         changeCalculator.computeMostEfficientChange(3)
     }
 
-    @Ignore
     @Test
     fun errorIfNoCombinationCanAddUpToTarget() {
         val changeCalculator = ChangeCalculator(listOf(5, 10))
@@ -88,7 +80,6 @@ class ChangeCalculatorTest {
         changeCalculator.computeMostEfficientChange(94)
     }
 
-    @Ignore
     @Test
     fun cannotFindNegativeChangeValues() {
         val changeCalculator = ChangeCalculator(listOf(1, 2, 5))
@@ -98,7 +89,6 @@ class ChangeCalculatorTest {
 
         changeCalculator.computeMostEfficientChange(-5)
     }
-
 }
 
 private inline fun <reified T> assertContainsExactly(actualList: List<T>, expectedValues: List<T>) {
