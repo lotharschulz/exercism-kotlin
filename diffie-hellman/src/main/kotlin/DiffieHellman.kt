0d5d667306b657f93ca86a55da96ecd279db6ac2
Lothar Schulz <lothar.sch@gmx.de>
2023-10-02 22:43:42 +0200

    chore diffieHellman all tests pass

--- diffie-hellman/src/main/kotlin/DiffieHellman.kt
+++ diffie-hellman/src/main/kotlin/DiffieHellman.kt
@@ -1,16 +1,15 @@
 import java.math.BigInteger
+import java.security.SecureRandom
 
 object DiffieHellman {
 
-    fun privateKey(prime: BigInteger): BigInteger {
-        return 20.toBigInteger()
-    }
+    fun privateKey(prime: BigInteger): BigInteger =
+        BigInteger.valueOf(SecureRandom().nextLong(2, prime.toLong()))
 
-    fun publicKey(p: BigInteger, g: BigInteger, privKey: BigInteger): BigInteger {
-        TODO("Implement the function to complete the task")
-    }
+    fun publicKey(p: BigInteger, g: BigInteger, privKey: BigInteger): BigInteger =
+        g.modPow(privKey, p)
 
-    fun secret(prime: BigInteger, publicKey: BigInteger, privateKey: BigInteger): BigInteger {
-        TODO("Implement the function to complete the task")
-    }
+    fun secret(prime: BigInteger, publicKey: BigInteger, privateKey: BigInteger): BigInteger =
+        publicKey.modPow(privateKey, prime)
+
 }
